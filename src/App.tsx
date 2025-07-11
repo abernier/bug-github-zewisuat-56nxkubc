efad74aca6db1bf491c2559a7a959f45756ce533
abernier <antoine.bernier@gmail.com>
2025-07-11 08:10:27 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import { ComponentProps, ComponentRef, useEffect, useRef, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { useRecorder, zip } from './useRecorder'
+
+const fps = 60
+
+export default function App() {
+  const [time, setTime] = useState(0)
+
+  return (
+    <>
+      <Canvas>
+        <Scene time={time} setTime={setTime} />
+      </Canvas>
+
+      <div style={{ position: 'fixed', top: 0, right: 0, padding: '.5rem' }}>
+        <label style={{ display: 'flex', alignItems: 'center' }}>
+          time:
+          <input
+            type="range"
+            min="0"
+            max="3"
+            step={1 / fps}
+            value={time}
+            onChange={(e) => {
+              setTime(Number(e.target.value))
+            }}
+          />
+        </label>
+      </div>
+
+      <p style={{ position: 'fixed', bottom: 0, left: 0, right: 0, textAlign: 'center' }}>Double-click cube to export scene</p>
+    </>
+  )
+}
+
+function Scene({ time, setTime }: { time: number; setTime: (time: number) => void }) {
+  const { record } = useRecorder(fps, setTime)
+
+  const handleClick = async () => {
+    const videoFrames = await record(2)
+    console.log('videoFrames', videoFrames)
+
+    // zip)
+    const zipBlob = await zip(videoFrames, 'png')
+    const zipUrl = URL.createObjectURL(zipBlob)
+    const a = Object.assign(document.createElement('a'), {
+      href: zipUrl,
+      download: 'frames.zip'
+    }).click()
+  }
+
+  return (
+    <>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box time={time} onDoubleClick={handleClick} />
+    </>
+  )
+}
+
+function Box({ time, ...props }: { time: number } & ComponentProps<'mesh'>) {
+  const ref = useRef<ComponentRef<'mesh'>>(null)
+
+  // useEffect(() => {
+  //   if (ref.current) {
+  //     ref.current.rotation.x = time
+  //   }
+  // }, [time])
+
+  return (
+    <mesh
+      ref={ref}
+      {...props}
+      rotation-x={time}
+      //
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  )
+}
---
+++ src/useRecorder.ts
@@ -0,0 +1,76 @@
+import { advance, invalidate, useThree, flushSync } from '@react-three/fiber'
+import JSZip from 'jszip'
+import { useCallback, useRef } from 'react'
+
+export function useRecorder(fps = 120, setTime: (time: number) => void) {
+  const state = useThree()
+  const dt = 1000 / fps
+  const clock = useRef(0) // ms
+
+  const record = useCallback(
+    (duration: number) => {
+      return new Promise<VideoFrame[]>((resolve, reject) => {
+        {
+          const totalFrames = Math.round(duration * fps)
+          const videoFrames: VideoFrame[] = []
+
+          while (videoFrames.length < totalFrames) {
+            const t = clock.current / 1000 // seconds
+
+            //
+            //
+            //
+
+            flushSync(() => setTime?.(t))
+            invalidate()
+            // await new Promise(requestAnimationFrame);
+
+            advance(clock.current)
+            state.gl.getContext().finish()
+
+            const videoFrame = new VideoFrame(state.gl.domElement, {
+              timestamp: clock.current * 1e3 // µs
+            })
+            videoFrames.push(videoFrame)
+
+            //
+            clock.current += dt // tick clock
+          }
+
+          resolve(videoFrames)
+        }
+      })
+    },
+    [fps, setTime, state, dt]
+  )
+
+  return { record }
+}
+
+export async function zip(frames: VideoFrame[], fmt = 'png') {
+  const zip = new JSZip()
+
+  for (let i = 0; i < frames.length; i++) {
+    const frame = frames[i]
+
+    const canvas = new OffscreenCanvas(frame.displayWidth, frame.displayHeight)
+    const ctx = canvas.getContext('2d')!
+    ctx.drawImage(frame, 0, 0)
+
+    // b. Transformer le canvas en Blob (image)
+    console.log(`Converting frame ${i} to image Blob...`)
+    // Note: `convertToBlob`
+    const blob = await canvas.convertToBlob({ type: `image/${fmt}` })
+    console.log(`Frame ${i} converted to Blob.`)
+
+    // c. Ajouter le fichier dans l’archive
+    const name = `frame_${String(i).padStart(4, '0')}.${fmt}`
+    zip.file(name, blob)
+
+    // d. Libérer la mémoire occupée par la VideoFrame
+    frame.close()
+  }
+
+  // 3. Générer l’archive ZIP (asynchrone)
+  return zip.generateAsync({ type: 'blob' })
+}
